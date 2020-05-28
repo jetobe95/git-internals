21e2ac432650dd28059de04631a52b9119e83605
Jeffrey Torres <jtorres@technisupport.com>
2020-05-28 15:48:02 -0500

    Dia 4

--- bisect/index.js
+++ bisect/index.js
@@ -2,3 +2,4 @@ console.log(`Empezando las funcionalidad #1`);
 console.log(`Correcciones en la funcionalidad #1`);
 console.log('bug 🐞');
 console.log('Avanzando en la funcionalidad #1 dia 3');
+console.log('Avanzando en la funcionalidad #1 dia 4');
